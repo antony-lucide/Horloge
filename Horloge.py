217b93d9e5b7eae6c50dbaa683549b25639e1b98
antony-lucide <antony.lucide@laplateforme.io>
2023-02-02 17:04:20 +0100

    Ceci est le rendu final du projet horloge pour la pool python.

---
+++ Horloge.py
@@ -0,0 +1,25 @@
+import time
+
+def Horloge():
+#Déclaration des variable avec les inputs pour le temps, en spécifiant que ce sont des nombres entiers
+        heure = int(input("Choissisez une heure de départ:"))
+        min = int(input("choissisez minute:"))
+        seconde = int(input("Choissisez secondes"))
+#Boucle infinie pour pouvoir rajouter du temps
+        while(True):
+#La propriété time sleep permet de faire passer des secondes dans ma boucle,les conditions sont la pour vérifier si le chrono devrait s'arrêter
+#Si ils s'arrêtent, alors remettre le chrono a zero et rajoute 1 à la valeur suivante, donc, seconde = min = heure, ainsi de suite
+#Ensuite on affichent le résultats et on formate  pour que la largeur s'arrête à 2 chiffres, par défaut 0
+                time.sleep(1)
+                if(seconde == 60):
+                        seconde = 0
+                        min+=1
+                if(min == 60):
+                        min=0
+                        heure+=1
+                if(heure == 24):
+                        heure = 0
+                seconde+=1
+                print("{:0>2}:{:0>2}:{:0>2}".format(heure,min,seconde))
+
+Horloge()
